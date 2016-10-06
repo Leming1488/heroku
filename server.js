737cb58437b227371cbf5a2d31854d0d201323c3
pkozelskiy <pkozelskiy@n-core.ru>
2016-10-06 16:39:10 +0300

    [react] packjson

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+app.use(express.static('public'));
+app.use( function (req, res, next) {
+  if (req.headers['x-forwarded-proto'] === 'http') {
+    next();
+  } else {
+    res.redirect('http://' +  req.hostname + req.url);
+  }
+});
+
+app.listen(port, () => console.log('server start' + PORT ));
