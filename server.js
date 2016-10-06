0564734d3087c6b34f93c5f7eb72478a0500d6b2
pkozelskiy <pkozelskiy@n-core.ru>
2016-10-06 16:57:27 +0300

    [react] правки сервера 2

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 const express = require('express');
+
 const app = express();
 
 const PORT = process.env.PORT || 3000;
-app.use(express.static('public'));
-app.use( function (req, res, next) {
+
+app.use(function (req, res, next) {
   if (req.headers['x-forwarded-proto'] === 'http') {
     next();
   } else {
     res.redirect('http://' +  req.hostname + req.url);
   }
 });
+
+
+app.use(express.static('public'));
+
 
 app.listen(PORT, () => console.log('server start' + PORT ));
